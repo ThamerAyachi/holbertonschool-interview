5600e81f383e0b45c705cb1d31d8b1e4594321ea
ThamerAyachi <thamerjak3@gmail.com>
2023-08-28 22:20:46 +0100

    add readme and comments

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Create new node
+ *
+ * @parent: Pointer to the node parent
+ * @value: value of node
+ *
+ * Return: Pointer of the new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new = malloc(sizeof(binary_tree_t));
---
+++ heap_insert/README.md
@@ -0,0 +1,3 @@
+# Heap Insert
+
+## Loading
