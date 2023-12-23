e9553a372cbb3611d3debe38e20adac71080de62
ThamerAyachi <thamerjak3@gmail.com>
2023-12-23 16:24:25 +0100

    try fix

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,18 +11,18 @@
  */
 void menger(int level)
 {
-    int col, row, size;
+	int col, row, size;
 
-    if (level >= 0)
-    {
-        size = pow(3, level);
-        for (col = 0; col < size; col++)
-        {
-            for (row = 0; row < size; row++)
-                printf("%c", printer(col, row));
-            printf("\n");
-        }
-    }
+	if (level >= 0)
+	{
+		size = pow(3, level);
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+				printf("%c", printer(col, row));
+			printf("\n");
+		}
+	}
 }
 
 /**
@@ -34,8 +34,8 @@ void menger(int level)
  */
 char printer(int col, int row)
 {
-    for (; col && row; col /= 3, row /= 3)
-        if (col % 3 == 1 && row % 3 == 1)
-            return (' ');
-    return ('#');
+	for (; col && row; col /= 3, row /= 3)
+		if (col % 3 == 1 && row % 3 == 1)
+			return (' ');
+	return ('#');
 }
