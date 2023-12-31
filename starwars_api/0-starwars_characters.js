cfee2bf6c064dd13f203daa7437026e2ceab1c90
ThamerAyachi <thamerjak3@gmail.com>
2023-12-31 13:23:58 +0100

    try fix task

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 const request = require("request");
 
 const URL = "https://swapi-api.hbtn.io/api/films/";
-request.get(URL + process.argv[1], async (error, response, body) => {
+request.get(URL + process.argv[2], async (error, response, body) => {
 	if (response.statusCode === 200 && error === null) {
 		const linkChars = JSON.parse(body).characters;
 		const linkCharsList = (chr) => {
