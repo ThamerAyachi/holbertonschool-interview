e2e2de868b5af825427f095ff688428cb1dc50be
ThamerAyachi <thamerjak3@gmail.com>
2024-01-26 09:30:29 +0100

    holbertonschool-interview

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,165 +1,105 @@
 #include "holberton.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0
+ * _puts - prints a string.
+ * @s: a string.
+ * Return: Nothing.
  */
-int main(int argc, char *argv[])
+void _puts(char *s)
 {
-	char *error = "Error\n";
-	int i;
-
-	if (argc != 3 || !is_a_number(argv[1]) || !is_a_number(argv[2]))
+	if (*s != '\0')
 	{
-		for (i = 0; error[i] != '\0'; i++)
-		{
-			_putchar(error[i]);
-		}
-		exit(98);
+		_putchar(*s);
+		puts(s + 1);
 	}
+}
+/**
+ * err_message - print s and exit 98 status
+ * @s: error message to print
+ * Return: Nothing
+ */
+void err_message(char *s)
+{
+	_puts(s);
+	exit(98);
+}
+/**
+ * _isdigit - check if s is a number or not.
+ * @s: string to check.
+ * Return: 0 if s is a number otherwise 1.
+ */
+int _isdigit(char *s)
+{
+	int i, digit = 0;
 
-	multiply(argv[1], argv[2]);
+	for (i = 0; s[i] && !digit; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			digit++;
+	}
+	return (digit);
+}
+/**
+ * operations - multiplies, adds and stores the result in a string.
+ * @num1: first number.
+ * @num2: second number.
+ * @len1: length of num1.
+ * @len2: length of num2.
+ * Return: result of multiplies.
+ */
+char *operations(char *num1, char *num2, int len1, int len2)
+{
+	char *result = NULL;
+	int i, j, carry, len_total = (len1 + len2);
+
+	result = malloc(sizeof(char) * len_total);
+	if (!result)
+		err_message("Error");
+	for (i = 0; i < len_total; i++)
+		result[i] = '0';
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			carry += (num1[i] - '0') * (num2[j] - '0');
+			carry += result[i + j + 1] - '0';
+			result[i + j + 1] = (carry % 10) + '0';
+			carry /= 10;
+		}
+		if (carry)
+			result[i + j + 1] = (carry % 10) + '0';
+	}
+	return (result);
+}
+/**
+ * main - multiplies two positive numbers.
+ * description: Usage: mul num1 num2
+ * Print the result, followed by a new line.
+ * @av: arguments value (num1, num2)
+ * @ac: arguments count
+ * Return: 0 if success otherwise 98 and print Error.
+ */
+int main(int ac, char **av)
+{
+	int len1 = 0, len2 = 0;
+	char *num1 = av[1], *num2 = av[2], *result = NULL;
+
+	if (ac != 3 || _isdigit(num1) || _isdigit(num2))
+		err_message("Error");
+	if (av[1][0] == 48 || av[2][0] == 48)
+		_puts("0"), exit(0);
+
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
+
+	result = operations(num1, num2, len1, len2);
+	if (result[0] == '0')
+		_puts(result + 1);
+	else
+		_puts(result);
+	free(result);
 	return (0);
-}
-
-/*corergir lo anterior, porq solo compara el primer caracter*/
-
-/**
- * multiply - Fills an array with the multiplication num_1 times i
- * Arguments
- * @num_1:	string representing the 1st number in 10 base
- * @num_2:  string representing the 2nd number in 10 base
- * Returns - The multiplication
- */
-
-void multiply(char *num_1, char *num_2)
-{
-	char mul[10][MAX1], answer[MAX1][MAX2];
-	int tag, i, j, len_1, len_2, res, value, pos_1, pos_2, row, z;
-
-	for (len_1 = str_len(num_1), len_2 = str_len(num_2), i = 0; i < 10; i++)
-		for (j = 0; j < MAX1; j++)
-			mul[i][j] = '.';
-
-	for (i = 0; i < MAX1; i++)
-		for (j = 0; j < MAX2; j++)
-			answer[i][j] = '.';
-
-	/* 1. Creating multiplication table*/
-	for (tag = 0, i = 0; i < 10; i++)
-	{
-		for (j = 0; j <= len_1; j++)
-		{
-			pos_1 = len_1 - j - 1;
-			if (pos_1 >= 0)
-			{
-				res = ((num_1[pos_1] - 48) * i) + tag;
-				value = res % 10;
-				mul[i][j] = (value + '0');
-				tag = res / 10;
-			}
-			else
-			{
-				mul[i][j] = (tag != 0) ? tag + 48 : '.';
-				tag = 0;
-			}
-		}
-	}
-
-	/* 2. Creating answer table*/
-	for (j = 0; j < len_2; j++)
-	{
-		pos_2 = len_2 - j - 1;
-		row = num_2[pos_2] - 48;
-		for (z = 0; z < len_1 + 1; z++)
-			answer[j][z + j] = mul[row][z];
-	}
-	sum_and_print(answer, len_1, len_2);
-}
-
-/**
- * is_a_number - Indicates if a string represents a number in 10 base
- * Arguments
- * @a_str: string representing a number in 10 base
- * Return: 1 if true, 0 if not.
- */
-int is_a_number(char *a_str)
-{
-	while (*a_str != '\0')
-	{
-		if (*a_str < '0' || *a_str > '9')
-			return (_FALSE_);
-		a_str++;
-	}
-	return (_TRUE_);
-}
-
-/**
- * sum_and_print - Generates the sumation and print the results
- * Arguments
- * @a:	matrix with multiplitation tables
- * @len_1: len of string 1
- * @len_2: len os string 2
- * Return: noting.
- */
-void sum_and_print(char a[MAX1][MAX2], int len_1, int len_2)
-{
-	int tag, i, j, sum, value;
-	char total[MAX2], ans[MAX2 - 1];
-	char *ar;
-
-	/* 3. Add each column from left to right */
-	tag = 0;
-	for (j = 0; j <= (len_1 + len_2); j++)
-	{
-		sum = 0;
-		for (i = 0; i < len_2; i++)
-			sum += (a[i][j] >= '0' && a[i][j] <= '9') ? a[i][j] - 48 : 0;
-		value = (sum + tag) % 10; /* las digit added */
-		total[j] = (value + '0'); /* converted to*/
-		tag = (sum + tag) / 10;
-	}
-
-	/* Reverse the string */
-	for (i = 0; i < len_1 + len_2; i++)
-	{
-		ans[i] = total[len_1 + len_2 - 1 - i];
-	}
-	ans[i] = '\0';
-
-	/* remove 0-s from the left side of the string */
-	value = str_len(ans);
-	for (i = 0; i < value; i++)
-	{
-		if (ans[i] == '0')
-			ar = &ans[i + 1];
-		else
-		{
-			ar = &ans[i];
-			i = value;
-		}
-	}
-	for (i = 0; ar[i] != '\0'; i++)
-	{
-		_putchar(ar[i]);
-	}
-	putchar('\n');
-}
-
-/**
- * str_len - Calculates the lenght of a string
- * Arguments
- * @a:	matrix with multiplitation tables
- * Return: The lengnth of a.
- */
-int str_len(char *a)
-{
-	int i;
-
-	for (i = 0; a[i] != '\0'; i++)
-	{
-	}
-	return (i);
 }
--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,18 +1,10 @@
-#ifndef _MULTI_H_
-#define _MULTI_H_
-
-#define _TRUE_ 1
-#define _FALSE_ 0
-#define MAX1 2500
-#define MAX2 2500
-
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/* LIBRARIES */
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+/* PROTOTYPES */
 int _putchar(char c);
-void multiply(char *num_1, char *num_2);
-int is_a_number(char *a_str);
-void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
-int str_len(char *a);
-
-#endif /*_MULTI_H_*/
+#endif /* _HOLBERTON_H_ */
