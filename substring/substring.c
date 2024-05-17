e25da169861e3bff9442c472201b19e4f28e0b9b
ThamerAyachi <thamerjak3@gmail.com>
2024-05-17 17:03:58 +0100

    Substring with concatenated words

--- substring/substring.c
+++ substring/substring.c
@@ -1,128 +1,158 @@
 #include "substring.h"
+#include <stdlib.h>
 
 /**
- * find_substring - finds all possible substrings containing list of words
- * @s: the string to scan
- * @words: the array of words all substrings must be concatenation of
- * @nb_words: the number of elements in the array words
- * @n: pointer to which to store the number of elements in the returned array
- *
- * Return: array storing each index in s at which a substring was found
- */
+ * find_substring - finds all possible substrings containing a list of words
+ * @s: string
+ * @words: array of words all substrings must be a concatenation arrangement of
+ * @nb_words: number of elements in the array words
+ * @n: pointer where number
+ * Return: an array storing each index in `s` at which a substring was found.
+ **/
+
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int word = 0, i = 0, match = 0, count = 0, j = 0, k = 0, I = 0;
-	int *word_used = NULL, *indices = NULL;
+	int i, j, *result = NULL, *seen, words_len, len;
+
+	if (!s || !words || !(*words) || !nb_words || !n)
+		return (NULL);
 
 	*n = 0;
-	if (words == NULL)
+
+	seen = malloc(sizeof(int) * nb_words);
+	if (!seen)
 		return (NULL);
-	j = (nb_words * string_length(words[0]));
-	i = allocate(&word_used, nb_words, &indices, (string_length(s) / j) + 1);
-	if (i == -1)
-		return (NULL);
-	for (i = 0; s[i]; i++)
+
+	words_len = _strlen(*words);
+	len = _strlen(s);
+	i = 0;
+	for (i = 0; (len - i) >= (words_len * nb_words); i++)
 	{
-		I = i;
-		for (reset(&word_used, nb_words), word = 0; word < nb_words && s[i]; word++)
+		for (j = 0; j < nb_words; j++)
+			seen[j] = 0;
+		if (is_substring(s + i, words, nb_words, words_len, seen))
 		{
-			match = string_compare(s, i, words[word]);
-			while (match != 0)
+			if (append(&result, n, i) == -1)
 			{
-				word_used[word] = 1;
-				i += match;
-				if (s[i] == '\0')
-					break;
-				for (count = 0, word = 0; word < nb_words; word++)
-				{
-					if (word_used[word] != -1)
-						count++;
-					match = string_compare(s, i, words[word]);
-					if (match != 0)
-						break;
-				}
-				if (count == nb_words)
-					indices[k++] = (i - j);
+				free(seen);
+				return (NULL);
 			}
 		}
-		if (i != I || s[i] == '\0')
-			i--;
 	}
-	free(word_used);
-	*n = k;
-	return (indices);
+	free(seen);
+	return (result);
 }
 
 /**
- * allocate - allocates memory for int arrays
- * @word_used: pointer to first memory to allocate
- * @nb_words: number of words to allocate space for
- * @indices: pointer to second memory to allocate
- * @max_index: maximum number of indices possible to allocate for
- *
- * Return: -1 if failed, 0 if successful
- */
-int allocate(int **word_used, int nb_words, int **indices, int max_index)
+ * is_substring - checks if a string is the start of a substring
+ * @s: string
+ * @words: words
+ * @nb_words: number of words
+ * @len: length of words
+ * @seen: helper array to keep track of which words have been seen
+ * Return: 1 if string is start of substring, otherwise 0
+ **/
+
+int is_substring(char const *s, char const **words,
+				 int nb_words, int len, int *seen)
 {
-	*word_used = malloc(sizeof(int) * nb_words);
-	if (*word_used == NULL)
-		return (-1);
-	reset(word_used, nb_words);
-	*indices = malloc(sizeof(int) * max_index);
-	if (*indices == NULL)
+	int i, all_seen = 1;
+
+	if (!(*s))
 	{
-		free(word_used);
+		for (i = 0; i < nb_words; i++)
+			if (seen[i] == 0)
+				return (0);
+		return (1);
+	}
+
+	for (i = 0; *s && i < nb_words; i++)
+	{
+		if (!seen[i] && !_strncmp(s, words[i], len))
+		{
+			seen[i] = 1;
+			return (is_substring(s + len, words, nb_words, len, seen));
+		}
+		if (seen[i] == 0)
+			all_seen = 0;
+	}
+
+	return (all_seen);
+}
+
+/**
+ * append - appends an integer to the end of an allocated integer array
+ * @arr: pointer to pointer to array
+ * @arr_size: pointer to array size
+ * @n: number to append to array
+ * Return: 0 on success, -1 on failure
+ **/
+
+int append(int **arr, int *arr_size, int n)
+{
+
+	if (!arr_size)
+		return (-1);
+
+	if (!arr)
+	{
+		*arr_size = 0;
 		return (-1);
 	}
-	reset(indices, max_index);
+
+	*arr_size += 1;
+	*arr = realloc(*arr, *arr_size * sizeof(int));
+
+	if (!(*arr))
+	{
+		*arr_size = 0;
+		return (-1);
+	}
+
+	(*arr)[*arr_size - 1] = n;
+
 	return (0);
 }
 
 /**
- * string_compare - compares two strings to see if one contains the other
- * @s1: the first string to compare, the longer of the strings
- * @i: the index of the first string to start from
- * @s2: the second string to compare, the shorter of the strings
- *
- * Return: length of the shorter word contained or 0 if no match
- */
-int string_compare(char const *s1, unsigned int i, char const *s2)
-{
-	unsigned int j = 0;
+ * _strncmp - custom strncmp
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of characters to compare
+ * Return: 0 if same, else difference between first different character
+ **/
 
-	for (j = 0; s1[i] && s2[j]; i++, j++)
-	{
-		if (s1[i] != s2[j])
-			return (0);
-	}
-	return (j);
+int _strncmp(char const *s1, char const *s2, int n)
+{
+	int i;
+
+	if (!n)
+		return (0);
+
+	for (i = 0; i < n && s1[i] && s2[i] && s1[i] == s2[i]; i++)
+		;
+
+	if (i == n || s1[i] == s2[i])
+		return (0);
+
+	return (s2[i] - s1[i]);
 }
 
 /**
- * string_length - finds the length of a string
- * @str: the string to get length of
- *
- * Return: length of the string
- */
-int string_length(char const *str)
-{
-	unsigned int i = 0;
+ * _strlen - custom strlen
+ * @s: string
+ * Return: length of string
+ **/
 
-	for (i = 0; str[i]; i++)
-		continue;
-	return (i);
-}
-
-/**
- * reset - sets all elements of array to -1
- * @array: pointer to the array to reset
- * @size: the size of the array to reset
- *
- */
-void reset(int **array, int size)
+int _strlen(char const *s)
 {
 	int i = 0;
 
-	for (i = 0; i < size; i++)
-		(*array)[i] = -1;
+	if (!s)
+		return (0);
+
+	while (s[i])
+		i++;
+
+	return (i);
 }
--- substring/substring.h
+++ substring/substring.h
@@ -1,15 +1,11 @@
-#ifndef SUBSTRING_H
-#define SUBSTRING_H
+#ifndef SUBSTR_H_
+#define SUBSTR_H_
 
-/* INCLUDED LIBRARIES */
-#include <stdio.h>
-#include <stdlib.h>
-
-/* FUNCTION PROTOTYPES */
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
-int allocate(int **word_used, int nb_words, int **indices, int max_index);
-int string_compare(char const *s1, unsigned int i, char const *s2);
-int string_length(char const *str);
-void reset(int **array, int size);
+int _strlen(char const *s);
+int _strncmp(char const *s1, char const *s2, int n);
+int append(int **arr, int *arr_size, int n);
+int is_substring(char const *s, char const **words,
+				 int nb_words, int len, int *seen);
 
 #endif
